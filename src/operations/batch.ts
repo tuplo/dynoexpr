0317a8ff99fd08a82e9b2aad5bce034d327acee1
Rui Costa <ruicosta042@gmail.com>
2020-07-07 00:05:19 +0100

    feat: handle batch and transaction requests
    fix #7

---
+++ src/operations/batch.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-param-reassign */
+import {
+  DynoexprInput,
+  BatchRequestInput,
+  BatchGetInput,
+  BatchWriteInput,
+  BatchRequestItemsInput,
+  BatchRequestOutput,
+} from 'dynoexpr';
+import { getSingleTableExpressions } from './single';
+
+export function isBatchRequest(
+  params: DynoexprInput | BatchRequestInput
+): params is BatchRequestInput {
+  return `RequestItems` in params;
+}
+
+function isBatchGetRequest(
+  tableParams: BatchGetInput | BatchWriteInput[]
+): tableParams is BatchGetInput {
+  return !Array.isArray(tableParams);
+}
+
+function isBatchWriteRequest(
+  tableParams: BatchGetInput | BatchWriteInput[]
+): tableParams is BatchWriteInput {
+  if (!Array.isArray(tableParams)) return false;
+  const [firstTable] = tableParams;
+  return `DeleteRequest` in firstTable || `PutRequest` in firstTable;
+}
+
+export function getBatchExpressions<
+  T extends BatchRequestOutput = BatchRequestOutput
+>(params: BatchRequestInput): T {
+  return {
+    ...params,
+    RequestItems: Object.entries(params.RequestItems).reduce(
+      (accParams, [tableName, tableParams]) => {
+        if (isBatchGetRequest(tableParams)) {
+          accParams[tableName] = getSingleTableExpressions(tableParams);
+        }
+        if (isBatchWriteRequest(tableParams)) {
+          accParams[tableName] = tableParams;
+        }
+        return accParams;
+      },
+      {} as BatchRequestItemsInput
+    ),
+  } as T;
+}
---
+++ src/operations/transact.ts
@@ -0,0 +1,27 @@
+import {
+  TransactRequestInput,
+  TransactRequestOutput,
+  TransactOperation,
+  DynoexprInput,
+} from 'dynoexpr';
+import { getSingleTableExpressions } from './single';
+
+export function isTransactRequest(
+  params: DynoexprInput | TransactRequestInput
+): params is TransactRequestInput {
+  return `TransactItems` in params;
+}
+
+export function getTransactExpressions<
+  T extends TransactRequestOutput = TransactRequestOutput
+>(params: TransactRequestInput): T {
+  return {
+    ...params,
+    TransactItems: params.TransactItems.map((tableItems) => {
+      const [key] = Object.keys(tableItems) as TransactOperation[];
+      return {
+        [key]: getSingleTableExpressions(tableItems[key]),
+      };
+    }),
+  } as T;
+}
