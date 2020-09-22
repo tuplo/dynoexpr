5e6058968d0db3a5ad4e824cc77ad62a2f464823
Rui Costa <ruicosta042@gmail.com>
2020-09-22 18:41:55 +0100

    feat: multiple update operations in one expression
    re #9

---
+++ src/expressions/update-ops.test.ts
@@ -0,0 +1,127 @@
+import type { UpdateInput } from 'dynoexpr';
+import {
+  getUpdateSetExpression,
+  getUpdateRemoveExpression,
+  getUpdateAddExpression,
+  getUpdateDeleteExpression,
+  getUpdateOperationsExpression,
+} from './update-ops';
+
+describe('update operations - SET/REMOVE/ADD/DELETE', () => {
+  it('builds a SET update expression', () => {
+    expect.assertions(1);
+    const params: UpdateInput = {
+      UpdateSet: {
+        foo: 'foo - 2',
+        bar: '2 - bar',
+        baz: 'baz + 9',
+      },
+    };
+    const result = getUpdateSetExpression(params);
+    const expected = {
+      UpdateExpression:
+        'SET #na4d8 = #na4d8 - :v862c, #n51f2 = :v862c - #n51f2, #n6e88 = #n6e88 + :vad26',
+      ExpressionAttributeNames: {
+        '#na4d8': 'foo',
+        '#n51f2': 'bar',
+        '#n6e88': 'baz',
+      },
+      ExpressionAttributeValues: {
+        ':v862c': 2,
+        ':vad26': 9,
+      },
+    };
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('builds a REMOVE update expression', () => {
+    expect.assertions(1);
+    const params: UpdateInput = {
+      UpdateRemove: {
+        foo: 'bar',
+        baz: 2,
+      },
+    };
+    const result = getUpdateRemoveExpression(params);
+    const expected = {
+      UpdateExpression: 'REMOVE #na4d8, #n6e88',
+      ExpressionAttributeNames: {
+        '#na4d8': 'foo',
+        '#n6e88': 'baz',
+      },
+    };
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('builds an ADD update expression', () => {
+    expect.assertions(1);
+    const params: UpdateInput = {
+      UpdateAdd: {
+        foo: 'bar',
+        baz: 2,
+      },
+    };
+    const result = getUpdateAddExpression(params);
+    const expected = {
+      UpdateExpression: 'ADD #na4d8 :v51f2, #n6e88 :v862c',
+      ExpressionAttributeNames: {
+        '#na4d8': 'foo',
+        '#n6e88': 'baz',
+      },
+      ExpressionAttributeValues: {
+        ':v51f2': 'bar',
+        ':v862c': 2,
+      },
+    };
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('builds a DELETE update expression', () => {
+    expect.assertions(1);
+    const params: UpdateInput = {
+      UpdateDelete: {
+        foo: 'bar',
+        baz: 2,
+      },
+    };
+    const result = getUpdateDeleteExpression(params);
+    const expected = {
+      UpdateExpression: 'DELETE #na4d8 :v51f2, #n6e88 :v862c',
+      ExpressionAttributeNames: {
+        '#na4d8': 'foo',
+        '#n6e88': 'baz',
+      },
+      ExpressionAttributeValues: {
+        ':v51f2': 'bar',
+        ':v862c': 2,
+      },
+    };
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('builds multiple update expressions', () => {
+    expect.assertions(1);
+    const params: UpdateInput = {
+      UpdateSet: { ufoo: 'ufoo - 2' },
+      UpdateRemove: { rfoo: 'rbar' },
+      UpdateAdd: { afoo: 'abar' },
+      UpdateDelete: { dfoo: 'dbar' },
+    };
+    const result = getUpdateOperationsExpression(params);
+    const expected = {
+      UpdateExpression:
+        'SET #nff12 = #nff12 - :v862c REMOVE #nff12, #n978b ADD #nb01c :ve948 DELETE #nd358 :v45cc',
+      ExpressionAttributeNames: {
+        '#n978b': 'rfoo',
+        '#nb01c': 'afoo',
+        '#nd358': 'dfoo',
+        '#nff12': 'ufoo',
+      },
+      ExpressionAttributeValues: {
+        ':v45cc': 'dbar',
+        ':ve948': 'abar',
+      },
+    };
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ src/expressions/update-ops.ts
@@ -0,0 +1,65 @@
+import type { UpdateInput, UpdateOutput } from 'dynoexpr';
+import { getUpdateExpression } from './update';
+
+type GetUpdateExpressionFn = (params?: UpdateInput) => UpdateOutput;
+
+export const getUpdateSetExpression: GetUpdateExpressionFn = (params = {}) => {
+  const { UpdateSet, ...restOfParams } = params;
+  return getUpdateExpression({
+    ...restOfParams,
+    Update: UpdateSet,
+    UpdateAction: 'SET',
+  });
+};
+
+export const getUpdateRemoveExpression: GetUpdateExpressionFn = (
+  params = {}
+) => {
+  const { UpdateRemove, ...restOfParams } = params;
+  return getUpdateExpression({
+    ...restOfParams,
+    Update: UpdateRemove,
+    UpdateAction: 'REMOVE',
+  });
+};
+
+export const getUpdateAddExpression: GetUpdateExpressionFn = (params = {}) => {
+  const { UpdateAdd, ...restOfParams } = params;
+  return getUpdateExpression({
+    ...restOfParams,
+    Update: UpdateAdd,
+    UpdateAction: 'ADD',
+  });
+};
+
+export const getUpdateDeleteExpression: GetUpdateExpressionFn = (
+  params = {}
+) => {
+  const { UpdateDelete, ...restOfParams } = params;
+  return getUpdateExpression({
+    ...restOfParams,
+    Update: UpdateDelete,
+    UpdateAction: 'DELETE',
+  });
+};
+
+export const getUpdateOperationsExpression: GetUpdateExpressionFn = (
+  params = {}
+) => {
+  const updateExpressions: string[] = [];
+  const outputParams = [
+    getUpdateSetExpression,
+    getUpdateRemoveExpression,
+    getUpdateAddExpression,
+    getUpdateDeleteExpression,
+  ].reduce((acc, getExpressionFn) => {
+    const expr = getExpressionFn(acc);
+    const { UpdateExpression = '' } = expr;
+    updateExpressions.push(UpdateExpression);
+    return expr;
+  }, params as UpdateOutput);
+
+  outputParams.UpdateExpression = updateExpressions.filter(Boolean).join(' ');
+
+  return outputParams;
+};
