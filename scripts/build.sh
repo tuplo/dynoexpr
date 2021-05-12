5f23c5e75545abc25e2c700702be4cdb24e849ab
Rui Costa <ruicosta042@gmail.com>
2021-05-12 23:37:51 +0100

    feat: bundle with esm format

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,5 @@
 
 rimraf dist
 tsc --build tsconfig.build.json
-esbuild src/index.ts --bundle --platform=node --outfile=dist/index.js --external:aws-sdk
-esbuild src/index.ts --bundle --outfile=dist/index.modern.js --external:aws-sdk --external:crypto
+esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js --external:aws-sdk --external:crypto
 cp src/dynoexpr.d.ts dist/dynoexpr.d.ts
