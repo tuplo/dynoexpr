5ae1b2e432a8dc78ed4de743394cf02344be152b
Rui Costa <ruicosta042@gmail.com>
2021-05-14 13:37:12 +0100

    feat: support for node>13.10

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 rimraf dist
+rimraf cjs
 tsc --build tsconfig.build.json
 
-esbuild src/index.cjs --bundle --platform=node --outfile=cjs/index.js \
+esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs \
   --external:aws-sdk
 esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js \
   --external:aws-sdk --external:crypto
+
+# node12 compatibility
+mkdir cjs && cp dist/index.cjs cjs/index.js
 
 cp src/dynoexpr.d.ts dist/dynoexpr.d.ts
